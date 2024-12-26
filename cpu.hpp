001bd23a49f384b44ff42b6c2423a74f77dd8d81
Jigglypuff JR_Noah <noah.mcmillan3@education.nsw.gov.au>
2024-12-26 12:28:58 +1100

    Update cpu.hpp

--- cpu.hpp
+++ cpu.hpp
@@ -1,103 +1,84 @@
+#include<string.h>
+#include<stdio.h>
+
+#include<../Headers/instructions.hpp>
+
 // Sizes
-using BYTE = char;
-using WORD = short;
-using RAM = char*;
+using BYTE = unsigned char;
+using WORD = unsigned short;
 
+// Register Types
+struct IR_Reg {
+    union { struct { BYTE I; BYTE R; }; WORD IR; };
+};
+
+struct registers {
+    union { struct { BYTE A; BYTE F; }; WORD AF; };
+    union { struct { BYTE B; BYTE C; }; WORD BC; };
+    union { struct { BYTE D; BYTE E; }; WORD DE; };
+    union { struct { BYTE H; BYTE L; }; WORD HL; };
+};
+
+// Cpu Unit
 struct CPU_UNIT {
+    // Emulator Properties
+    BYTE incorrectCodeByte=0;
+    WORD incorrectCodeWord=0;
+
     // Registers
-    union statusReg {
-        struct {
-            BYTE S: 1;
-            BYTE Z: 1;
-            BYTE unused1: 1;
-            BYTE H: 1;
-            BYTE unused0: 1;
-            BYTE PV: 1;
-            BYTE N: 1;
-            BYTE C: 1;
-        };
-        BYTE flags;
-    };
+    registers regA;
+    registers regB;
 
-    struct regs {
-        union { struct { BYTE B; BYTE C; }; WORD BC;};
-        union { struct { BYTE D; BYTE E; }; WORD DE;};
-        union { struct { BYTE H; BYTE L; }; WORD HL;};
-        union { struct { BYTE A; statusReg F; }; WORD AF;}; // Accumulator + Flags
-    };
+    registers currReg = regA;
 
-    struct s_regs {
-        BYTE I, R;
-        WORD IX, IY, SP, PC;
-    };
+    WORD PC=0;
+    WORD SP=0;
 
-    // Components
-    regs bRegs;
-    regs aRegs;
-    s_regs sRegs;
-    regs currReg = bRegs;
-    RAM ram;
+    WORD IX=0;
+    WORD IY=0;
+    
+    IR_Reg IR;
 
-    WORD addrBus;
-    BYTE dataBus;
+    // Register Functions
+    // Get Byte Register
+    BYTE* getByteReg(char registerName) {
+        if (registerName=='A') return &currReg.A;
+        else if (registerName=='B') return &currReg.B;
+        else if (registerName=='C') return &currReg.C;
+        else if (registerName=='D') return &currReg.D;
+        else if (registerName=='E') return &currReg.E;
+        else if (registerName=='H') return &currReg.H;
+        else if (registerName=='L') return &currReg.L;
 
-    void initCPU(RAM outerRAM) {
-        for (int i=0; i<sizeof(outerRAM); i++) ram[i]==outerRAM[i];
+        return &incorrectCodeByte;
+    }
+    // Get Word Register
+    WORD* getWordReg(char* registerName) {
+        if (strcmp("AF", registerName)) return &currReg.AF;
+        else if (strcmp("BC", registerName)) return &currReg.BC;
+        else if (strcmp("DE", registerName)) return &currReg.DE;
+        else if (strcmp("HL", registerName)) return &currReg.HL;
 
-        sRegs.I=sRegs.R=0;
-        sRegs.IX=sRegs.IY=sRegs.SP=sRegs.PC=0;
-        bRegs.F.flags=aRegs.F.flags=0;
-        bRegs.BC=bRegs.DE=bRegs.HL=0;
-        aRegs.BC=aRegs.DE=aRegs.HL=0;
+        return &incorrectCodeWord;
     }
 
-    BYTE fetchByte(WORD cycles) {
-        int *ptr = &cycles;
-
-        BYTE data = dataBus;
-        sRegs.PC++;
-        *ptr-=4;
+    BYTE fetchByte(WORD &cycles, RAM_UNIT ram) {
+        BYTE data = ram.readAddress(PC);
+        PC++;
+        cycles-=3;
         return data;
     }
 
-    // Opcodes
-    static constexpr BYTE INS_INC_B = 0x04;
-    static constexpr BYTE INS_INC_C = 0x14;
-    static constexpr BYTE INS_INC_D = 0x24;
-    static constexpr BYTE INS_INC_E = 0x34;
-    static constexpr BYTE INS_INC_H = 0x44;
-    static constexpr BYTE INS_INC_L = 0x54;
-    static constexpr BYTE INS_INC_HL_P = 0x64;
-    static constexpr BYTE INS_INC_A = 0x74;
-
-    static constexpr BYTE INS_INC_BC = 0x03;
-    static constexpr BYTE INS_INC_DE = 0x13;
-    static constexpr BYTE INS_INC_HL = 0x23;
-    static constexpr BYTE INS_INC_SP = 0x33;
-
-    void startCycle(WORD cycles) {
+    void startCycle(WORD cycles, RAM_UNIT ram) {
         while (cycles>0) {
-            printf("%d\n", cycles);
-            BYTE instr = fetchByte(cycles);
-            switch (instr)
-            {
-            case INS_INC_B: currReg.B++; break;
-            case INS_INC_C: currReg.C++; break;
-            case INS_INC_D: currReg.D++; break;
-            case INS_INC_E: currReg.E++; break;
-            case INS_INC_H: currReg.H++; break;
-            case INS_INC_L: currReg.L++; break;
-            case INS_INC_HL_P: ram[currReg.HL]++; break;
-            case INS_INC_A: currReg.A++; break;
+            BYTE opcode = fetchByte(cycles, ram);
+            cycles--;
 
-            case INS_INC_BC: currReg.BC++; break;
-            case INS_INC_DE: currReg.DE++; break;
-            case INS_INC_HL: currReg.HL++; break;
-            case INS_INC_SP: sRegs.SP++; break;
-            
-            default:
-                break;
-            }
+            printf("Byte %d: %d\n", PC-1, opcode);
+
+            // Decode the opcode
         }
+        
     }
+
 };
