ad82959785788c7d66c774eff4a529d9cdb0c10f
Jigglypuff JR_Noah <noah.mcmillan3@education.nsw.gov.au>
2024-12-26 12:29:19 +1100

    Update ram.hpp

--- ram.hpp
+++ ram.hpp
@@ -1,18 +1,45 @@
 #include<stdlib.h>
+#include<stdio.h>
 
 // Sizes
-using BYTE = char;
-using WORD = short;
-using RAM = char*;
+using BYTE = unsigned char;
+using WORD = unsigned short;
 
 // Ram Unit
 struct RAM_UNIT {
-    RAM initRam(WORD size) {
-        return (RAM)malloc(((size_t)size)+1);
+    WORD size = 0;
+    BYTE* ram = (BYTE*)malloc(0);
+    bool init = false;
+
+    // Frees Ram; Best ran at end of CPU usage
+    void freeRam() {
+        if (init) {
+            size = 0;
+
+            free(ram);
+            ram=NULL;
+            init=false;
+        }
     }
 
-    void clearRam(RAM ram) {
-        free(ram);
-        ram=NULL;
+    // Reads a byte from Ram; Must be initialized first
+    BYTE readAddress(WORD addr) {
+        if (init && addr<=size) {
+            return ram[addr];
+        } else exit(1);
+    }
+
+    // Writes a byte to Ram; Must be initialized first
+    void writeAddress(WORD addr, BYTE value) {
+        if (init && addr<size) {
+            ram[addr]=value;
+        } else exit(1);
     }
 };
+
+// Allocates space for Ram; Initializes Ram
+void initRam(RAM_UNIT &ramModule, WORD bytes) {
+    ramModule.size = bytes;
+    ramModule.ram = (BYTE*)realloc(ramModule.ram, ramModule.size+1);
+    ramModule.init = true;
+}
