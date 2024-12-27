820c7d41ba701da67581acbbdfc981ac7b20c436
Jigglypuff JR_Noah <noah.mcmillan3@education.nsw.gov.au>
2024-12-27 17:13:35 +1100

    Update instructions.cpp

--- instructions.cpp
+++ instructions.cpp
@@ -1,5 +1,9 @@
-// Types
-using BYTE = char;
+// Size
+using CONST_BYTE = const unsigned char;
 
 // Instructions
-BYTE INS_NOP[2] = {0x00, 0}; // Opcode, Additional Bytes
+CONST_BYTE instructionsLength = 1;
+
+CONST_BYTE INSTRUCTIONS[1][2] = {  // Opcode, Additional Bytes
+    {0x69, 0}, // NOP
+};
